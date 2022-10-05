57e481dfca9a387199147f2ed92222f7d7d8671b
XaeneXX <91166271+XaeneXX@users.noreply.github.com>
2022-10-05 17:02:23 +0800

    Create IR_sensor_testing.ino

---
+++ IR_sensor_testing.ino
@@ -0,0 +1,17 @@
+int L_sensor 39
+int R_sensor=36;
+
+
+void setup() {
+
+Serial.begin(9600);
+pinMode(R_sensor, INPUT);
+}
+
+void loop() {
+
+Serial.println(digitalRead(L_sensor));
+Serial.println("  ");
+Serial.println(digitalRead(R_sensor));
+delay(500);
+}
